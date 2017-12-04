50b64ccc73a733cff6b3b419ffc1677110ada19a
Simon <simon.mateika@hotmail.com>
2017-12-04 23:05:58 +0200

    fix

--- resources/views/functions/categories/index.blade.php
+++ resources/views/functions/categories/index.blade.php
@@ -1,54 +1,51 @@
 @extends('layouts.admin')
-
 @section('content')
-
-
 <body>
-
-<div class="container">
-  <div class="row mt-4">
-    <div class="col-md">
-      <div class="card">
-        <div class="card-header text-white bg-success mb-3">
-          Add Categories
-        </div>
-
-        
-        <div class="card-body">
-        <div class="row">
-        <div class="col-md-6">
-       
-        {!! Form::open(['route' => 'categories.store', 'method' => 'post', 'class' => 'form-group']) !!}
-        {!! Form::text('name', '', array('class' => 'form-control')) !!}
-        {!! Form::submit('Add', array('class' => 'btn btn-success mt-2')) !!}
-        {!! Form::close() !!}
+  <div class="container">
+    <div class="row mt-4">
+      <div class="col-md">
+        <div class="card">
+          <div class="card-header text-white bg-success mb-3">
+            Add Categories
+          </div>
           
-        </div>
-
-
-        <div class="col-md-6">
-          <ul class="list-group">
-            @foreach ($categories as $category)
-              <li class="list-group-item d-flex justify-content-between align-items-center">
-               <strong>{{ $category->name }}</strong> 
-              <span class="">
-                <a href="{{ route('categories.edit', $category->id) }}" class="fa fa-edit"></a>
-                <em class="ml-3"></em>
-                <a href="{{ route('categories_destroy', $category->id) }}" class="fa fa-trash"></a>
-              </span>
-              </li>
-            @endforeach
-          </ul>
-        </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-md-6">
+                
+                {!! Form::open(['route' => 'categories.store', 'method' => 'post', 'class' => 'form-group']) !!}
+                <div class="input-group">
+                  <input type="text" class="form-control" placeholder="Role name..." name="name">
+                  <span class="input-group-addon">
+                    <button type="submit" class="btn btn-neutral btn-sm now-ui-icons ui-1_simple-add">
+                    </button>
+                  </span>
+                </div>
+                
+                {{--         {!! Form::text('name', '', array('class' => 'form-control')) !!}
+                {!! Form::submit('Add', array('class' => 'btn btn-success mt-2')) !!} --}}
+                {!! Form::close() !!}
+                
+              </div>
+              <div class="col-md-6">
+                <ul class="list-group">
+                  @foreach ($categories as $category)
+                  <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <strong>{{ $category->name }}</strong>
+                    <span class="">
+                      <a href="{{ route('categories.edit', $category->id) }}" class="fa fa-edit"></a>
+                      <em class="ml-3"></em>
+                      <a href="{{ route('categories_destroy', $category->id) }}" class="fa fa-trash"></a>
+                    </span>
+                  </li>
+                  @endforeach
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
-
-
 </body>
-
-
 @endsection
--- resources/views/functions/items/index.blade.php
+++ resources/views/functions/items/index.blade.php
@@ -1,150 +1,51 @@
-@extends('layouts.admin')
+@extends('layouts.user')
 @section('content')
-<div class="container">
-	<div class="row mt-4">
-		<div class="col-md-8 mx-auto">
-			<div class="row">
-				<div class="col-md">
-					<ul class="nav nav-pills text-center" id="pills-tab" role="tablist">
+<body style="background-image:url(../assets/img/login.jpg);" >
+<div class="section-tabs" style="margin-top: 10rem;">
+	<div class="container">
+		<div class="row">
+			<div class="col-md col-lg-10 col-xl-8 ml-auto mr-auto">
+				<p class="category">Upload your story...</p>
+				<div class="card">
+					<ul class="nav nav-tabs nav-tabs-neutral justify-content-center" role="tablist" data-background-color="orange">
 						<li class="nav-item">
-							<a class="nav-link" id="pills-text-tab" data-toggle="pill" href="#pills-text" role="tab" aria-controls="pills-text" aria-selected="true">
-								<i class="fa fa-file-text" aria-hidden="true"></i>
+							<a class="nav-link active" data-toggle="tab" href="#home" role="tab">
+								<i class="now-ui-icons objects_umbrella-13"></i> Image
 							</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" id="pills-image-tab" data-toggle="pill" href="#pills-image" role="tab" aria-controls="pills-iamge" aria-selected="false">
-								<i class="fa fa-file-image-o" aria-hidden="true"></i>
+							<a class="nav-link" data-toggle="tab" href="#profile" role="tab">
+								<i class="now-ui-icons shopping_cart-simple"></i> Text
 							</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" id="pills-video-tab" data-toggle="pill" href="#pills-video" role="tab" aria-controls="pills-video" aria-selected="false">
-								<i class="fa fa-file-video-o" aria-hidden="true"></i>
+							<a class="nav-link" data-toggle="tab" href="#messages" role="tab">
+								<i class="now-ui-icons shopping_shop"></i> Sound
 							</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" id="pills-music-tab" data-toggle="pill" href="#pills-music" role="tab" aria-controls="pills-music" aria-selected="false">
-								<i class="fa fa-music" aria-hidden="true"></i>
+							<a class="nav-link" data-toggle="tab" href="#settings" role="tab">
+								<i class="now-ui-icons ui-2_settings-90"></i> Video
 							</a>
 						</li>
 					</ul>
-				</div>
-			</div>
-			<div class="row mt-4">
-				<div class="col-md">
-					<div class="tab-content" id="pills-tabContent">
-						<div class="tab-pane fade" id="pills-text" role="tabpanel" aria-labelledby="pills-text-tab">
-							<div class="row">
-								<div class="col-md">
-									<div class="card">
-										<div class="card-header">Add Text</div>
-										<div class="card-body">
-											{!! Form::open(['route' => 'categories.store', 'method' => 'post', 'class' => 'form-group']) !!}
-											<div class="row">
-												<div class="col-md">
-													{!! Form::text('name-text', '', array('class' => 'form-control', 'placeholder' => 'Title')) !!}
-												</div>
-											</div>
-											<div class="row">
-												<div class="col-md">
-													{!! Form::textarea('text', '', array('class' => 'form-control mt-2', 'placeholder' => 'Your creation...')) !!}
-												</div>
-											</div>
-											<div class="row">
-												<div class="col-md">
-													{!! Form::submit('Add', array('class' => 'btn btn-success mt-2')) !!}
-													{!! Form::close() !!}
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
+					<div class="card-body">
+						<!-- Tab panes -->
+						<div class="tab-content">
+							<div class="tab-panel" id="home" role="tabpanel">
+	
 							</div>
-						</div>
-						<div class="tab-pane fade" id="pills-image" role="tabpanel" aria-labelledby="pills-image-tab">
-							<div class="row">
-								<div class="col-md">
-									<div class="card">
-										<div class="card-header">Add Image</div>
-										<div class="card-body">
-											{!! Form::open(['route' => 'categories.store', 'method' => 'post', 'class' => 'form-group']) !!}
-											<div class="row">
-												<div class="col-md">
-													{!! Form::text('name-image', '', array('class' => 'form-control', 'placeholder' => 'Title')) !!}
-												</div>
-											</div>
-											<div class="row">
-												<div class="col-md mx-auto">
-													<label class="custom-file mt-2">
-														{!! Form::file('image', array('class' => 'custom-file-input', 'placeholder' => 'Image to upload...')) !!}
-														<span class="custom-file-control"></span>
-													</label>
-												</div>
-											</div>
-											<div class="row">
-												<div class="col-md">
-													{!! Form::submit('Add', array('class' => 'btn btn-success mt-2')) !!}
-													{!! Form::close() !!}
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
+							<div class="tab-pane" id="profile" role="tabpanel">
+								<p> I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. 
+								</p>
 							</div>
-						</div>
-						<div class="tab-pane fade" id="pills-video" role="tabpanel" aria-labelledby="pills-video-tab">
-							<div class="row">
-								<div class="col-md">
-									<div class="card">
-										<div class="card-header">Add Video</div>
-										<div class="card-body">
-											{!! Form::open(['route' => 'categories.store', 'method' => 'post', 'class' => 'form-group']) !!}
-											<div class="row">
-												<div class="col-md">
-													{!! Form::text('name-video', '', array('class' => 'form-control', 'placeholder' => 'Title')) !!}
-												</div>
-											</div>
-											<div class="row">
-												<div class="col-md">
-													{!! Form::text('video-link', '', array('class' => 'form-control mt-2', 'placeholder' => 'Youtube video link')) !!}
-												</div>
-											</div>
-											<div class="row">
-												<div class="col-md">
-													{!! Form::submit('Add', array('class' => 'btn btn-success mt-2')) !!}
-													{!! Form::close() !!}
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
+							<div class="tab-pane" id="messages" role="tabpanel">
+								<p>I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. So when you get something that has the name Kanye West on it, it’s supposed to be pushing the furthest possibilities. I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus.</p>
 							</div>
-						</div>
-						<div class="tab-pane fade" id="pills-music" role="tabpanel" aria-labelledby="pills-music-tab">
-							<div class="row">
-								<div class="col-md">
-									<div class="card">
-										<div class="card-header">Add Video</div>
-										<div class="card-body">
-											{!! Form::open(['route' => 'categories.store', 'method' => 'post', 'class' => 'form-group']) !!}
-											<div class="row">
-												<div class="col-md">
-													{!! Form::text('name-video', '', array('class' => 'form-control', 'placeholder' => 'Title')) !!}
-												</div>
-											</div>
-											<div class="row">
-												<div class="col-md">
-													{!! Form::text('video-link', '', array('class' => 'form-control mt-2', 'placeholder' => 'Soundcloud song link')) !!}
-												</div>
-											</div>
-											<div class="row">
-												<div class="col-md">
-													{!! Form::submit('Add', array('class' => 'btn btn-success mt-2')) !!}
-													{!! Form::close() !!}
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
+							<div class="tab-pane" id="settings" role="tabpanel">
+								<p>
+									"I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at."
+								</p>
 							</div>
 						</div>
 					</div>
